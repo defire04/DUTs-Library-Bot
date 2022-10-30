268cb2fc5afef1dcd6c9568d6fd46981424ec3f5
Artandrey <artandrey7777@gmail.com>
2022-10-30 21:26:42 +0200

    added threader perfomance counter

--- threader.py
+++ threader.py
@@ -6,10 +6,16 @@ from time import sleep
 
 
 
-def run_one_by_one (func1, func2, finish, link):
+def run_one_by_one (func1, func2, finish):
     result1 = func1()
-    func2(link)
+    func2(result1)
     finish()
+
+class Task: 
+    def __init__ (self, taskFunction, callback, name = 'unnamed_task'):
+        self.function = taskFunction
+        self.callback = callback
+        self.name = name
 
 class Threader:
     def __init__ (self, threads_count):
@@ -17,8 +23,8 @@ class Threader:
         self.currnet_running_count = 0
         self.tasks = []
         self.total = 0
-    def add_task (self, func, callback, link):
-        self.tasks.append( [func, callback, link] )
+    def add_task (self, func, callback):
+        self.tasks.append( Task(func, callback) )
         if self.currnet_running_count < self.threads_count:
             self.run_new_task()
     
@@ -26,12 +32,12 @@ class Threader:
         if len(self.tasks):
             task = self.tasks.pop(0)
             self.currnet_running_count += 1
-            thread = threading.Thread(target=run_one_by_one, args=(task[0], task[1], self.on_finish, task[2]))
+            thread = threading.Thread(target=run_one_by_one, args=(task.function, task.callback, self.on_finish))
             thread.start()
     def on_finish (self):
         self.currnet_running_count -= 1
         self.total += 1
-        print(str((self.total / 1942) * 100) + '%\t' + str(self.total))
+        # print(str((self.total / 1942) * 100) + '%\t' + str(self.total))
         if self.currnet_running_count < self.threads_count:
             self.run_new_task()
 

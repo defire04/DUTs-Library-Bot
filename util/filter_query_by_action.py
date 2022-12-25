6361d07c109211276d025d2e7d743f8c56678f84
Artandrey <artandrey7777@gmail.com>
2022-12-25 20:11:06 +0200

    bugfix / fixed bug in conditional check in create_filter_query_by_action

--- util/filter_query_by_action.py
+++ util/filter_query_by_action.py
@@ -6,7 +6,7 @@ def create_filter_query_by_action(action_id: int, filtering_callback: FunctionTy
     def filter_query_by_action(callback):
         action = ButtonAction.from_json(callback.data)
         check_result = callback.data and action.id == action_id
-        if not filtering_callback:
+        if not filtering_callback or not check_result:
             return check_result
         return filtering_callback(action)
 

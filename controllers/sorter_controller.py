9c70897b9c6f0e269f64d52a54da2e7870bc2dca
Dima Olaba <defire04st@gmail.com>
2022-12-26 22:22:29 +0200

    function / create sort books by year

---
+++ controllers/sorter_controller.py
@@ -0,0 +1,9 @@
+from typing import List
+
+from models.book import Book
+
+
+class Sorter:
+    @staticmethod
+    def sort_by_year(books: List[Book]):
+        return sorted(books, key=lambda book: book.year_of_publication)

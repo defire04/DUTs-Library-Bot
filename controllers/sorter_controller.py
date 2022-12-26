e34f7fff4c4715e23bef584e3b1071994a4cc6bc
Dima Olaba <defire04st@gmail.com>
2022-12-26 23:10:13 +0200

    function / create sort_by_year_reverse

--- controllers/sorter_controller.py
+++ controllers/sorter_controller.py
@@ -7,3 +7,7 @@ class Sorter:
     @staticmethod
     def sort_by_year(books: List[Book]):
         return sorted(books, key=lambda book: book.year_of_publication)
+
+    @staticmethod
+    def sort_by_year_reverse(books: List[Book]):
+        return books.reverse()

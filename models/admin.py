8f7ce5529f29e5e4256d803dc1dd6a87748f87ac
Dima Olaba <defire04st@gmail.com>
2022-12-27 15:02:45 +0200

    bugfix / local

--- models/admin.py
+++
@@ -1,8 +0,0 @@
-from resources import config
-
-
-class Admin:
-    def __init__(self, admin_id, admin_username):
-        self.admin_id = admin_id
-        self.admin_username = admin_username
-
--- models/user.py
+++ models/user.py
@@ -1,11 +1,22 @@
+
+
 class User:
-    def __init__(self, user_id, username, full_name):
+    def __init__(self, user_id: int, username: str, full_name: str, role: str):
         self.user_id = user_id
         self.username = username
         self.full_name = full_name
+        self.role = role
 
     @classmethod
     def create(cls, id, user_id, username, full_name):
         user = cls(user_id, username, full_name)
         user.id = id
         return user
+
+
+class Admin(User):
+
+    def __init__(self, admin_id: int, admin_username: str, role: str):
+        self.role = "ADMIN"
+        super().__init__(admin_id, admin_username, self.role)
+

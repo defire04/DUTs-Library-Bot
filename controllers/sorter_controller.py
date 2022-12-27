e833bffb1d790fbccdd11abfcdf102f171b443da
Artandrey <artandrey7777@gmail.com>
2022-12-27 15:02:45 +0200

    function / added date to year sortiong

--- controllers/sorter_controller.py
+++ controllers/sorter_controller.py
@@ -1,13 +1,16 @@
 from typing import List
 
+from datetime import date
 from models.book import Book
 from controllers.book_controller import BookController
 
 
 
 def sort_year(book):
+    current_year = date.today().year
+
     if not book.year_of_publication:
-        return 9283721983173
+        return current_year + 1
     return  book.year_of_publication
 
 class Sorter:
